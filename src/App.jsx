b6f4f41c07ea01374a54f0640e620cbffdea29ec
Patick Ramos <patrick.ramos@ciit.edu.ph>
2025-06-14 11:00:05 +0800

    set up app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { BrowserRouter, Route, Routes, NavLink, Link, Navigate } from 'react-router-dom'
+import React from 'react'
 
-function App() {
-  const [count, setCount] = useState(0)
+//page content
+import LogIn from './pages/LogIn'
+import Home from './pages/Home'
+import AboutUs from './pages/AboutUs'
+import ContactUs from './pages/ContactUs'
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
 
-export default App
+<div className='App'>
+  <BrowserRouter>
+    <nav>
+      <h1>Week-6 Lab</h1>
+      <NavLink to="/">LogIn</NavLink>
+      <NavLink to="/">HomePage</NavLink>
+      <NavLink to="/">AboutUs</NavLink>
+      <NavLink to="/">ContactUs</NavLink>
+    </nav>
+
+    <Routes>
+      <Route>
+        
+      </Route>
+    </Routes>
+
+  </BrowserRouter>
+</div>
